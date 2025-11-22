0a1545ce3c5aeabb82377aec3c969eb6e61cc91f
adhikari80 <adhikaribhaskar8@gmail.com>
2025-11-23 09:09:10 +1300

    Create activity_week1.py

---
+++ activity_week1.py
@@ -0,0 +1,15 @@
+name = "John Doe" #this is string.
+age = 28  #this is integer
+skills = ["Python", "SQL", "Power BI"] #this is a list of skills
+education = ("BSC Computer Science", 2020) #this is a tuple.
+contact_details = {"email": "123xyz@example.com", "phone": "98112237898"}#this is a dictionary//key-value pair.
+certifications = {"Azure", "AWS", "Azure"}#this is a set//one Azure is removed.
+
+print("Name of student:", name)
+print("Age of student:", age)
+print("Skills of student:", skills)
+print("Education background:", education)
+print("Contact Details:", contact_details)
+print("Certifications:", certifications)
+
+
