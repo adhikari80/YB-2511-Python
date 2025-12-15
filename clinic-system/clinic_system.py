efe0bad3f4bd6537053e7d64a750482f60b3b9fc
adhikari80 <adhikaribhaskar8@gmail.com>
2025-12-15 18:41:36 +1300

    code

--- clinic-system/clinic_system.py
+++ clinic-system/clinic_system.py
@@ -6,7 +6,7 @@ class ClinicSystem:
         self.cursor = self.conn.cursor()
         self.create_tables()
 
-    # ---------------- CREATE TABLES ----------------
+    # CREATE TABLES
     def create_tables(self):
         self.cursor.execute("""
         CREATE TABLE IF NOT EXISTS Patient (
@@ -39,7 +39,7 @@ class ClinicSystem:
         """)
         self.conn.commit()
 
-    # ---------------- INSERT DATA ----------------
+    # INSERT DATA 
     def add_patient(self, name, age, gender, contact):
         self.cursor.execute(
             "INSERT INTO Patient (full_name, age, gender, contact_number) VALUES (?, ?, ?, ?)",
@@ -54,7 +54,7 @@ class ClinicSystem:
         )
         self.conn.commit()
 
-    # ---------------- REQUIRED FUNCTIONS ----------------
+    # REQUIRED FUNCTIONS
     def list_senior_patients(self):
         print("\n--- Senior Patients (Age > 65) ---")
         self.cursor.execute("SELECT * FROM Patient WHERE age > 65")
@@ -74,26 +74,26 @@ class ClinicSystem:
         count = self.cursor.fetchone()[0]
         print("\nTotal Doctors Specialised in Ophthalmology:", count)
 
-    # ---------------- CLOSE CONNECTION ----------------
+    # CLOSE CONNECTION
     def close(self):
         self.conn.close()
 
 
-# ---------------- MAIN PROGRAM ----------------
+# MAIN PROGRAM
 if __name__ == "__main__":
     clinic = ClinicSystem()
 
-    # ---- Sample Patients ----
+    # Sample Patients 
     clinic.add_patient("Ram Sharma", 70, "Male", "9851222000")
     clinic.add_patient("Sita Koirala", 60, "Female", "9843299890")
     clinic.add_patient("Hari Adhikari", 75, "Male", "9038879893")
 
-    # ---- Sample Doctors ----
+    # Sample Doctors
     clinic.add_doctor("Dr. Smith", "Ophthalmology", "022-111111")
     clinic.add_doctor("Dr. Brown", "Cardiology", "022-222222")
     clinic.add_doctor("Dr. Lee", "Ophthalmology", "022-333333")
 
-    # ---- Required Outputs ----
+    # Required Outputs
     clinic.list_senior_patients()
     clinic.count_ophthalmology_doctors()
 
